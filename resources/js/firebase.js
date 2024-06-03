37f419465f113dd5f81b4c79662a7c382c3fdc79
yooeuddeum <145522980+yooeuddeum@users.noreply.github.com>
2024-06-03 17:29:18 +0900

    20240603

--- resources/js/firebase.js
+++ resources/js/firebase.js
@@ -6,6 +6,9 @@ import {
   setDoc,
   doc,
   addDoc,
+  deleteDoc,
+  getDoc,
+  updateDoc,
 } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -29,27 +32,36 @@ async function getDatas(collectionName) {
 }
 
 async function addDatas(collectionName, dataObj) {
-  // 문서 id 수동
-
-  //   try {
-  //     const saveDoc = await doc(db, collectionName, "2");
-  //     console.log(`doc() 결과: ${saveDoc}`);
-  //     const saveResult = await setDoc(saveDoc, dataObj);
-  //     console.log(`setDoc() 결과: ${saveResult}`);
-  //     return true;
-  //   } catch (error) {
-  //     return false;
-  //   }
-  // }
-
-  // 문서 id 자동
   try {
-    const collect = collection(db, collectionName);
-    await addDoc(collect, dataObj);
+    // 문서 ID 수동
+    // const saveDoc = await doc(db, collectionName, '3');
+    // console.log(`doc() 결과: ${saveDoc}`);
+    // const saveResult = await setDoc(saveDoc, dataObj);
+    // console.log(`setDoc() 결과: ${saveResult}`);
+
+    // 문서 ID 자동
+    const collect = await collection(db, collectionName);
+    await addDoc(collect, dataObj); // 결과 == undefined
     return true;
   } catch (error) {
     return false;
   }
 }
 
-export { db, getDatas, addDatas };
+async function deleteDatas(collectionName, docId) {
+  const docRef = await doc(db, collectionName, docId);
+  await deleteDoc(docRef);
+}
+
+// async function updateDatas() {
+//   doc(db, 컬렉션명, 문서id);
+//   getDoc(문서레퍼런스);
+//   updateDoc(문서데이터, 수정할 정보객체);
+// }
+async function updateDatas(collectionName, docId, updateInfoObj) {
+  const docRef = await doc(db, collectionName, docId);
+  // const docData = await getDoc(docRef);
+  await updateDoc(docRef, updateInfoObj);
+}
+
+export { db, getDatas, addDatas, deleteDatas, updateDatas };
